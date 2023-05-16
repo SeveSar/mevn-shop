5409e6f555e73ca5f643acc87977e88103ba4a44
Averin Artem <averin.dev@mail.ru>
2023-05-16 11:23:46 +0500

    fix space

--- src/components/ui/BaseButton.test.js
+++ src/components/ui/BaseButton.test.js
@@ -6,7 +6,6 @@ import BaseButton from "./BaseButton.vue";
 // в нём описывается  сам тест
 test("renders base button", () => {
   //3 состовляющие теста
-
   // given
   const options = {
     slots: {
